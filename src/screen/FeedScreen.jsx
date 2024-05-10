2455724f76ba0bd8dde6009cd00233b469459379
MehmetGassaloglu <Mehmet.gassaloglu1@bahcesehir.edu.tr>
2024-05-10 23:09:26 +0300

    Added Tasks, Feed, Course Screens and added an radio button to signup page

--- src/screen/FeedScreen.jsx
+++ src/screen/FeedScreen.jsx
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function FeedScreen  ()  {
   return (
